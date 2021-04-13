4737d29a6558ea31d1b6ac1e82902c0960b03b82
dstearle <dallas.st.earle@gmail.com>
2021-04-13 16:11:37 -0700

    Implemented method for toggling the reminder status of a task

--- src/components/Tasks.vue
+++ src/components/Tasks.vue
@@ -2,7 +2,7 @@
 
     <div :key="task.id" v-for="task in tasks">
 
-        <Task @delete-task="$emit('delete-task', task.id)" :task="task" />
+        <Task @toggle-reminder="$emit('toggle-reminder', task.id)" @delete-task="$emit('delete-task', task.id)" :task="task" />
 
     </div>
 
@@ -22,7 +22,7 @@
             
         },
 
-        emits: ['delete-task'],
+        emits: [ 'delete-task', 'toggle-reminder', ],
 
         props: {
 
