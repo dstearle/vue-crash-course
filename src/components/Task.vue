9413cd7349bed6303623ea75ad6312773767ae27
dstearle <dallas.st.earle@gmail.com>
2021-04-13 14:36:50 -0700

    Added the date for the task and some styling

--- src/components/Task.vue
+++ src/components/Task.vue
@@ -1,6 +1,13 @@
 <template>
 
-    <h3>{{ task.text }}</h3>
+    <div class="task">
+
+        <!-- Title -->
+        <h3>{{ task.text }}</h3>
+
+        <p>{{ task.day }}</p>
+
+    </div>
 
 </template>
 
@@ -19,3 +26,24 @@
     }
 
 </script>
+
+<style scope>
+
+    .task {
+    background: #f4f4f4;
+    margin: 5px;
+    padding: 10px 20px;
+    cursor: pointer;
+    }
+
+    .task.reminder {
+    border-left: 5px solid green;
+    }
+
+    .task h3 {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    }
+
+</style>
