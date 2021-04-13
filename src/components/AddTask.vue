cc33498490d2bd46e6643f4d7e4f42f16b81862c
dstearle <dallas.st.earle@gmail.com>
2021-04-13 16:20:16 -0700

    Created new AddTask component

---
+++ src/components/AddTask.vue
@@ -0,0 +1,52 @@
+<template>
+
+  <form @submit="onSubmit" class="add-form">
+
+    <!-- Task Title Input -->
+    <div class="form-control">
+
+        <label>Task</label>
+
+        <input type="text" v-model="text" name="text" placeholder="Add Task" />
+
+    </div>
+
+    <!-- Day & Time Input -->
+    <div class="form-control">
+
+      <label>Day & Time</label>
+
+      <input
+        type="text"
+        v-model="day"
+        name="day"
+        placeholder="Add Day & Time"
+      />
+
+    </div>
+
+    <!-- Set Reminder Input -->
+    <div class="form-control form-control-check">
+
+      <label>Set Reminder</label>
+
+      <input type="checkbox" v-model="reminder" name="reminder" />
+
+    </div>
+
+    <!-- Submit Button -->
+    <input type="submit" value="Save Task" class="btn btn-block" />
+
+  </form>
+
+</template>
+
+<script>
+
+    export default {
+
+        name: 'AddTask',
+        
+    }
+
+</script>
